2670dd8b24f86daeab512be28b365d3aaa827260
dandritsanos <dandritsanos@gmail.com>
2026-08-05 01:14:14 +0200

    test: add singular test asserting yearly shares sum to 100%

---
+++ dbt/tests/assert_electrification_shares_sum_to_100.sql
@@ -0,0 +1,6 @@
+select
+    registration_year,
+    round(sum(share_of_year_pct), 2) as total_share_pct
+from {{ ref('mart_fleet_electrification') }}
+group by registration_year
+having abs(sum(share_of_year_pct) - 100.0) > 0.1
