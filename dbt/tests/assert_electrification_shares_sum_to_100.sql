cafe4fa9fd9b228fe7fe074beb609668b3cf6ec4
dandritsanos <dandritsanos@gmail.com>
2026-08-26 23:42:45 +0300

    debuging the snapshots

--- dbt/tests/assert_electrification_shares_sum_to_100.sql
+++ dbt/tests/assert_electrification_shares_sum_to_100.sql
@@ -3,4 +3,4 @@ select
     round(sum(share_of_year_pct), 2) as total_share_pct
 from {{ ref('mart_fleet_electrification') }}
 group by registration_year
-having abs(sum(share_of_year_pct) - 100.0) > 
+having abs(sum(share_of_year_pct) - 100.0) > 0.02
